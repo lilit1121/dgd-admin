76ed544526fcaf529cc364327ddfcd5ea153ba12
anna-spec <anna.mnatsakanyan.84@mail.ru>
2024-10-30 12:31:17 +0400

    date filter 2

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -32,27 +32,6 @@ class PostController extends Controller
     }
 
 
-//    public function showPosts(Request $request)
-//    {
-//        $search = $request->input('search');
-//        $startDate = $request->input('start_date');
-//        $endDate = $request->input('end_date');
-//
-//        $query = Post::where('post_type', 'post');
-//
-//        if ($search) {
-//            $query->where('post_title', 'like', '%' . $search . '%');
-//        }
-//
-//        if ($startDate && $endDate) {
-//            $query->whereBetween('created_at', [$startDate, $endDate]);
-//        }
-//
-//        $posts = $query->orderBy('created_at', 'desc')->paginate(33);
-//
-//        return response()->json($posts);
-//    }
-
     public function showPost($id)
     {
         $posts = Post::where('id', $id)->get();
